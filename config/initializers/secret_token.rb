de322d9a3d10b85fe1689f8ab113937fb4700695
Richie Dan <richie.dan90@gmail.com>
2014-01-05 17:57:08 +0900

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pozz::Application.config.secret_key_base = 'ceae9ba184305a0c2c62cee240f08adcf800fc29b23801123d14c5f34386e8c0929e8b5b7f650bf323e92278f098fcbb134ae0009d9b69f9a882a69df85f1c88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pozz::Application.config.session_store :cookie_store, key: '_pozz_session'
